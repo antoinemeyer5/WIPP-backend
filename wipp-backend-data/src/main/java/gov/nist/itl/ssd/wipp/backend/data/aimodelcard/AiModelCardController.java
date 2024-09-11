97701201d9d809d5b7ffc2b5769c89482e74e03b
ajm32 <antoine.meyer@nist.gov>
2024-09-11 09:09:18 -0400

    feat: display full URL of training data, not just the ID

--- wipp-backend-data/src/main/java/gov/nist/itl/ssd/wipp/backend/data/aimodelcard/AiModelCardController.java
+++ wipp-backend-data/src/main/java/gov/nist/itl/ssd/wipp/backend/data/aimodelcard/AiModelCardController.java
@@ -54,6 +54,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -99,6 +100,11 @@ public class AiModelCardController {
         List<String> exposedHead = List.of("content-disposition");
         head.setAccessControlExposeHeaders(exposedHead);
         return head;
+    }
+
+    private Map<String, String> addFullUrlToId(Map<String, String> map) {
+        map.replaceAll((k,v)->v="/images-collection/"+v);
+        return map;
     }
 
     @RequestMapping(
@@ -155,7 +161,7 @@ public class AiModelCardController {
                 mc.getAuthor()
         );
         hf.setModel_type(mc.getOperationType());
-        hf.setTraining_data(mc.getTrainingData());
+        hf.setTraining_data(addFullUrlToId(mc.getTrainingData()));
         hf.setTesting_metrics(mc.getTesting());
 
         byte[] bytes = convertIntoBytes(hf);
@@ -198,7 +204,7 @@ public class AiModelCardController {
                 mc.getOperationType()
         );
         bii.setTimestamp(mc.getDate());
-        bii.setTraining_data(mc.getTrainingData());
+        bii.setTraining_data(addFullUrlToId(mc.getTrainingData()));
 
         byte[] bytes = convertIntoBytes(bii);
 
@@ -220,6 +226,7 @@ public class AiModelCardController {
     public ResponseEntity<byte[]> cdcs(@PathVariable("id") String id) throws IOException
     {
         AiModelCard mc = getAiModelCard(id);
+        mc.setTrainingData(addFullUrlToId(mc.getTrainingData()));
 
         byte[] bytes = convertIntoBytes(mc);
 
